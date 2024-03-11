0c1fb60cbdb2157551cac9a021dc87162c1dec6b
Elias <elabd.elias@gmail.com>
2024-03-12 00:07:02 +0100

    controllers config

---
+++ back-end/controllers/jobs.js
@@ -0,0 +1,39 @@
+const Thing = require('../models/Thing');
+
+exports.createThing = (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  console.log(req.body);
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Job added successfully!' }))
+    .catch(error => {
+      console.error('Error saving data:', error);
+      res.status(400).json({ error: error.message });
+    });
+};
+
+exports.modifyThing = (req, res, next) => {
+  Thing.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
+    .then(() => res.status(200).json({ message: 'Job modifié !' }))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.deleteThing = (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id})
+    .then(() => res.status(200).json({ message: 'Job supprimé !'}))
+    .catch(error => res.status(404).json({ error }));
+};
+
+exports.getOneThing = (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(404).json({ error }));
+};
+
+exports.getAllThings = (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+};
--- back-end/routes/jobs.js
+++ back-end/routes/jobs.js
@@ -1,43 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const Thing = require('../models/Thing');
+const jobsCtrl = require('../controllers/jobs');
 
-router.post('/', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  });
-  console.log(req.body);
-  thing.save()
-    .then(() => res.status(201).json({ message: 'Job added successfully!' }))
-    .catch(error => {
-      console.error('Error saving data:', error);
-      res.status(400).json({ error: error.message });
-    });
-});
+router.post('/', jobsCtrl.createThing);
 
-router.get('/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(404).json({ error }));
-});
+router.get('/:id', jobsCtrl.getOneThing);
 
-router.get('/', (req, res, next) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
+router.get('/', jobsCtrl.getAllThings);
 
-router.put('/:id', (req, res, next) => {
-  Thing.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
-    .then(() => res.status(200).json({ message: 'Job modifié !' }))
-    .catch(error => res.status(400).json({ error }));
-});
+router.put('/:id', jobsCtrl.modifyThing);
 
-router.delete('/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id})
-    .then(() => res.status(200).json({ message: 'Job supprimé !'}))
-    .catch(error => res.status(404).json({ error }));
-})
+router.delete('/:id', jobsCtrl.deleteThing);
 
 module.exports = router;
