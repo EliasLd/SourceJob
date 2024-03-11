775052b1107c847dc606b2e02f6855260c5e7df7
Elias <elabd.elias@gmail.com>
2024-03-11 23:52:28 +0100

    routing routine deported

--- back-end/app.js
+++ back-end/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+const jobsRoutes = require('./routes/jobs');
 
 const app = express();
 
-const Thing = require('./models/Thing');
-
-mongoose.connect('mongodb+srv://Elias:W4dJeX8jI4xQWeom@sourcejob.fd2djbg.mongodb.net/?retryWrites=true&w=majority&appName=SourceJob',
+mongoose.connect(`mongodb+srv://Elias:W4dJeX8jI4xQWeom@sourcejob.fd2djbg.mongodb.net/?retryWrites=true&w=majority&appName=SourceJob`,
 {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -21,43 +22,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.post('/api/jobs', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  });
-  console.log(req.body);
-  thing.save()
-    .then(() => res.status(201).json({ message: 'Job added successfully!' }))
-    .catch(error => {
-      console.error('Error saving data:', error);
-      res.status(400).json({ error: error.message });
-    });
-});
-
-app.get('/api/jobs/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.get('/api/jobs', (req, res, next) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.put('/api/jobs/:id', (req, res, next) => {
-  Thing.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
-    .then(() => res.status(200).json({ message: 'Job modifié !' }))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.delete('/api/jobs/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id})
-    .then(() => res.status(200).json({ message: 'Job supprimé !'}))
-    .catch(error => res.status(404).json({ error }));
-})
+app.use('/api/jobs', jobsRoutes);
 
 module.exports = app;
 
