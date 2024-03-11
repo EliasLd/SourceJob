540d00c540e151b88a790e9f85cf76ebc2302e55
Elias <elabd.elias@gmail.com>
2024-03-12 00:43:21 +0100

    added login logic

--- back-end/controllers/user.js
+++ back-end/controllers/user.js
@@ -16,5 +16,24 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-
+    User.findOne({ email: req.body.email})
+        .then(user => {
+            if(user === null){
+                res.status(401).json({ message: 'Email ou mot de passe incorrecte'});
+            } else {
+                bcrypt.compare(req.body.password, user.password)
+                    .then(valid => {
+                        if(!valid) {
+                            res.status(401).json({ message: 'Email ou de mot de passe incorrecte'})
+                        } else {
+                            res.status(200).json({
+                                userId: user._id,
+                                token: 'TOKEN',
+                            })
+                        }
+                    })
+                    .catch(error => res.status(500).json({ error }));
+            }
+        })
+        .catch(error => {res.status(500).json({ error })});
 };
