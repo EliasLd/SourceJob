f1d3a539700df5f6d8694e23f9f89b3c955863bc
Elias <elabd.elias@gmail.com>
2024-03-19 13:12:34 +0100

    changed logos on jobs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,10 +10,10 @@ export default function Navbar() {
                 <Link to='/api/jobs'>
                   <img src={logo} className='w-10 h-10'/>
                 </Link>
-                <div className='flex gap-x-4 xxs:gap-x-10 items-center'>
-                    <Link to='/api/jobs/addJob' className='font-inter font-semibold text-sm xxs:text-base  p-3 hover:border-b-2 hover:border-b-black'>Ajouter un job</Link>
-                    <Link to='/api/jobs' className='font-inter font-semibold text-sm xxs:text-base p-3 hover:border-b-2 hover:border-b-black'>Voir les jobs</Link> 
-                    <Link to='/api/profile' className='font-inter font-semibold text-sm xxs:text-base p-3 hover:border-b-2 hover:border-b-black'>Profil</Link>  
+                <div className='flex gap-x-4 xxs:gap-x-10 items-center'> 
+                    <Link to='/api/jobs/addJob' className='font-inter font-semibold text-sm xxs:text-base  p-3 hover:border-b-2 transition ease-in-out duration-500 hover:border-b-black'>Ajouter un job</Link>
+                    <Link to='/api/jobs' className='font-inter font-semibold text-sm xxs:text-base p-3 hover:border-b-2 transition ease-in-out duration-500 hover:border-b-black'>Voir les jobs</Link> 
+                    <Link to='/api/profile' className='font-inter font-semibold text-sm xxs:text-base p-3 hover:border-b-2 transition ease-in-out duration-500 hover:border-b-black'>Profil</Link>  
                 </div>
             </div>
         </div>
