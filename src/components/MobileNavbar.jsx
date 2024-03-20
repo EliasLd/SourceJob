903f7ab0303058128a7d7b530859c4aac853faea
Elias <elabd.elias@gmail.com>
2024-03-20 09:20:33 +0100

    styled mobile navbar

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
 import hamburger from '../assets/hamburger.svg';
+import cross from '../assets/cross.svg';
 
 export default function MobileNavbar() {
 
@@ -9,23 +10,28 @@ export default function MobileNavbar() {
 
     const toggleMenu = () => {
         setOpened(!opened);
-        console.log('Menu affiché');
     }
 
     return (
         <nav className='sticky top-0 z-10 bg-white border border-slate-200'>
             <div className='max-w-4xl mx-auto px-4 relative'>
                 <div className='flex items-center justify-between h-16'>
-                    <img src={hamburger} onClick={toggleMenu} className='w-10 h-10' alt='Hamburger Icon' />
+                    <img src={opened ? cross : hamburger} onClick={toggleMenu} className='w-10 h-10' alt='Hamburger Icon' />
                     <Link to='/api/jobs'>
                         <img src={logo} className='w-10 h-10' alt='Logo' />
                     </Link>
                     {opened && (
                         <div className="absolute top-full left-1/2 transform w-full -translate-x-1/2 bg-white border border-slate-200 p-2">
-                            <ul className='flex flex-row justify-center gap-x-8'>
-                                <li><Link to="/api/jobs">Jobs </Link></li>
-                                <li><Link to="/api/jobs/addJob">Ajouter </Link></li>
-                                <li><Link to="/api/profile">Profil </Link></li>
+                            <ul className='flex flex-row justify-center gap-x-2'>
+                                <div className='rounded-full p-1 bg-slate-100'>
+                                    <li><Link to="/api/jobs" className='mx-6 font-inter'>Jobs </Link></li>
+                                </div>
+                                <div className='rounded-full p-1 bg-slate-100'>
+                                    <li><Link to="/api/jobs/addJob" className='mx-6 font-inter'>Ajouter </Link></li>
+                                </div>
+                                <div className='rounded-full p-1 bg-slate-100'>
+                                    <li><Link to="/api/profile" className='mx-4 font-inter'>Profil </Link></li>
+                                </div>
                             </ul>
                         </div>
                     )}
